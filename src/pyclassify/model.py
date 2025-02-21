a71832c2addc2dbd68ee161ee8fb4bda6bda5ea0
alexserra98 <aserra@sissa.it>
2025-02-21 16:19:57 +0100

    practical4

--- src/pyclassify/model.py
+++ src/pyclassify/model.py
@@ -10,17 +10,17 @@ class AlexNet(nn.Module):
             nn.ReLU(inplace=True),
             nn.MaxPool2d(kernel_size=3, stride=2),
             
-            nn.Conv2d(64, 192, kernel_size=5, padding=2),
+            nn.Conv2d(64, 192, kernel_size=5, padding=2, groups=2),
             nn.ReLU(inplace=True),
             nn.MaxPool2d(kernel_size=3, stride=2),
             
-            nn.Conv2d(192, 384, kernel_size=3, padding=1),
+            nn.Conv2d(192, 384, kernel_size=3, padding=1, groups=2),
             nn.ReLU(inplace=True),
             
-            nn.Conv2d(384, 256, kernel_size=3, padding=1),
+            nn.Conv2d(384, 256, kernel_size=3, padding=1, groups=2),
             nn.ReLU(inplace=True),
             
-            nn.Conv2d(256, 256, kernel_size=3, padding=1),
+            nn.Conv2d(256, 256, kernel_size=3, padding=1, groups=2),
             nn.ReLU(inplace=True),
             nn.MaxPool2d(kernel_size=3, stride=2)
         )
